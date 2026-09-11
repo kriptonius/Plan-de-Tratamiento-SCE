2d4807286106bc76a576b50e66dd892f6cb91735
kriptonius <93629385+kriptonius@users.noreply.github.com>
2026-09-11 18:16:00 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = "plantratamiento-v1";
+const SHELL = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./cie10.js",
+  "./odontograma.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  // No interceptar llamadas a Supabase ni otros orígenes (login, DNI, etc.)
+  if (req.method !== "GET" || new URL(req.url).origin !== self.location.origin) return;
+
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy));
+      return res;
+    }).catch(() => cached))
+  );
+});
